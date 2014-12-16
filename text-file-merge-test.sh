4f6276274492cf2c3787d3bbe5e7a2f0d3b99a92
Carlos Eduardo B. Duarte <cebarcellos@gmail.com>
2014-12-15 23:55:29 -0200

    Test edition conflict, parallel edition w/o conflict and removal conflict.
    3 simple text files are created on one VM and it waits until the other one
    to be synced. Thus the test starts. Each test changes the same file
    simultaneously on both VMs.
    
    * Edition conflict test change the same line differently on each VM.
    It aims to test a conflict detection.
    * Parallel edition test change different lines on each VM.
    It aims to test if there is merge support.
    * Removal conflict test change a line on one VM and remove the file on the
    another one.
    It aims to test a conflict detection.

--- text-file-merge-test.sh
+++ text-file-merge-test.sh
@@ -22,10 +22,10 @@ fi
 VM1_IP=$1
 VM2_IP=$2
 SYNC_DIR=$3
-VM1_LINE="line 5 edited by $VM1_IP"
-VM2_LINE="line 5 edited by $VM2_IP"
 FILE_TMP=`mktemp` || exit 1
-FILE_SYNC=file-merge-test.txt
+FILE_EDIT_CONFLICT=file-edit-conflict-test.txt
+FILE_RM_CONFLICT=file-remove-conflict-test.txt
+FILE_PARALLEL_EDIT=file-parallel-edit-test.txt
 trap "rm -f $FILE_TMP" 0 1 2 3 15
 
 # Create a simple text file with 10 lines
@@ -36,15 +36,65 @@ do
 done
 
 # Copy the new file to both VMs
-scp -q $FILE_TMP $VM1_IP:"$SYNC_DIR/$FILE_SYNC"
-scp -q $FILE_TMP $VM2_IP:"$SYNC_DIR/$FILE_SYNC"
+scp -q $FILE_TMP $VM1_IP:"$SYNC_DIR/$FILE_EDIT_CONFLICT"
+scp -q $FILE_TMP $VM1_IP:"$SYNC_DIR/$FILE_RM_CONFLICT"
+scp -q $FILE_TMP $VM1_IP:"$SYNC_DIR/$FILE_PARALLEL_EDIT"
 
+# Wait until both VMs are synced
+echo Waiting for VMs to be synced
+files_synced=0
+file_idx=0
+sleep 3
+while [ $files_synced != 3 ]
+do
+	file_idx=$(($file_idx + 1))
+	if [ $file_idx = 0 ] ; then
+		file=$SYNC_DIR/$FILE_EDIT_CONFLICT
+	elif [ $file_idx = 1 ] ; then
+		file=$SYNC_DIR/$FILE_RM_CONFLICT
+	else
+		file=$SYNC_DIR/$FILE_PARALLEL_EDIT
+		file_idx=0
+	fi
+
+	if ssh ${VM2_IP} stat $file \> /dev/null 2\>\&1 ; then
+		files_synced=$(($files_synced + 1))
+		echo "$files_synced file(s) synced on VM $VM2_IP"
+	fi
+	sleep 1
+done
+
+# Conflicting edition
+vm1_line="line 5 edited by $VM1_IP"
+vm2_line="line 5 edited by $VM2_IP"
 # Edit it from inside of VM1. Edit line 5.
-ssh ${VM1_IP} "sed -i \"5s/.*/$VM1_LINE/\" $SYNC_DIR/$FILE_SYNC
-		exit"
+ssh ${VM1_IP} "sed -i \"5s/.*/$vm1_line/\" $SYNC_DIR/$FILE_EDIT_CONFLICT
+		exit" &
 # Edit it from inside of VM2. Edit line 5.
-ssh ${VM2_IP} "sed -i \"5s/.*/$VM2_LINE/\" $SYNC_DIR/$FILE_SYNC
-		exit"
+ssh ${VM2_IP} "sed -i \"5s/.*/$vm2_line/\" $SYNC_DIR/$FILE_EDIT_CONFLICT
+		exit" &
+wait
+
+# Conflicting remove
+vm1_line="line 5 edited by $VM1_IP"
+# Edit it from inside of VM1. Edit line 5.
+ssh ${VM1_IP} "sed -i \"5s/.*/$vm1_line/\" $SYNC_DIR/$FILE_RM_CONFLICT
+		exit" &
+# Edit it from inside of VM2. Edit line 5.
+ssh ${VM2_IP} "rm $SYNC_DIR/$FILE_RM_CONFLICT
+		exit" &
+wait
+
+# Parallel edition w/o conflict
+vm1_line="line 4 edited by $VM1_IP"
+vm2_line="line 5 edited by $VM2_IP"
+# Edit it from inside of VM1. Edit line 4.
+ssh ${VM1_IP} "sed -i \"4s/.*/$vm1_line/\" $SYNC_DIR/$FILE_PARALLEL_EDIT
+		exit" &
+# Edit it from inside of VM2. Edit line 5.
+ssh ${VM2_IP} "sed -i \"5s/.*/$vm2_line/\" $SYNC_DIR/$FILE_PARALLEL_EDIT
+		exit" &
+wait
 
 exit 0
 
