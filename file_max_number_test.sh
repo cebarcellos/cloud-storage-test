c6f66718d37a7acb11d2393365b4f8a092717f85
Carlos Eduardo B. Duarte <cebarcellos@gmail.com>
2014-12-16 00:03:17 -0200

    Change the max file number test to be faster to sync.
    1000 files per directory. Without it the syncing is too slow.

--- file_max_number_test.sh
+++ file_max_number_test.sh
@@ -5,31 +5,49 @@
 
 cd $1
 
-echo ""
-num_of_files_created=0
-num_of_1st_file=100001
-num_of_last_file=200000
+num_of_1st_file=1
+num_of_last_file=20000
+new_dir=0001000
 
+echo -e "\nCreating files"
+
+num_of_files_created=0
 for file_num in $(seq -f "%07g" $num_of_1st_file $num_of_last_file)
 do
 	file_name=file_${file_num}.txt
-	echo -n $file_name > $file_name
 
-	num_of_files_created=$((num_of_files_created + 1))
 	if [ $(($num_of_files_created % 1000)) == 0 ] ; then
 		echo -e -n "$file_name\r"
+		new_dir=$(($num_of_files_created + 1000))
+		new_dir=`printf "%07d" $new_dir`
+		mkdir -p $new_dir
 	fi
+
+	echo -n $file_name > $new_dir/$file_name
+	num_of_files_created=$((num_of_files_created + 1))
 done
 echo $num_of_files_created files created.
 date
 
-# Check integrity
+echo -e "\nChecking integrity"
+
+num_of_files_checked=0
 for file_num in $(seq -f "%07g" $num_of_1st_file $num_of_last_file)
 do
 	file_name=file_${file_num}.txt
-	file_content=`cat $file_name`
+
+	if [ $(($num_of_files_checked % 1000)) == 0 ] ; then
+		echo -e -n "$file_name\r"
+		new_dir=$(($num_of_files_checked + 1000))
+		new_dir=`printf "%07d" $new_dir`
+	fi
+
+	file_content=`cat $new_dir/$file_name`
 
 	if [ "$file_name" != "$file_content" ] ; then
 		echo ! Error creating file ${file_name}.
 	fi
+	num_of_files_checked=$((num_of_files_checked + 1))
 done
+echo Done
+
